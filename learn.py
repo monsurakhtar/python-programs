2511378990c674a29cbc56b6e6e966ed985b0fb0
monsurakhtar <monsurakhtar@gmail.com>
2023-03-25 21:03:47 +0530

    25-03-2023 programs

---
+++ learn.py
@@ -0,0 +1,6 @@
+print("Hello World")
+print("bhai video bhi like kar dena",end=" ")
+print("Subscribe to our channel")
+#my name is monsur akhtar laskar. I live in Kolkata
+"""This is our first Python class"""
+print("bhai video share kar dena","Subscribe to my channel")
---
+++ march-25.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print("the diff of",num1, " & ", num2 ,"is =", num1-num2)
+
